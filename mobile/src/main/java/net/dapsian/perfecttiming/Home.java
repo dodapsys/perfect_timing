3fb6fac0a494395b7b738dda8107cb0bdbb3c23d
dodapsys <dapsys.domas@gmail.com>
2016-10-23 10:16:37 +0300

    design update

--- mobile/src/main/java/net/dapsian/perfecttiming/Home.java
+++ mobile/src/main/java/net/dapsian/perfecttiming/Home.java
@@ -3,6 +3,7 @@ package net.dapsian.perfecttiming;
 import java.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.widget.TextView;
 
@@ -12,19 +13,21 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        //setSupportActionBar(myToolbar);
         Calendar cal = Calendar.getInstance();
-        String day = new String(String.valueOf(cal.get(Calendar.DAY_OF_MONTH)));
-        String year = new String(String.valueOf(cal.get(Calendar.YEAR)));
+        String day = String.valueOf(cal.get(Calendar.DAY_OF_MONTH));
+        String year = String.valueOf(cal.get(Calendar.YEAR));
         TextView date = (TextView)findViewById(R.id.date);
         date.setText(Html.fromHtml(day + "<sup><small>" + prefix(cal.get(Calendar.DAY_OF_MONTH)) + "</sup></small>" + "   " + month(cal.get(Calendar.MONTH))));
-        String weekn = new String(String.valueOf(cal.get(Calendar.WEEK_OF_YEAR)));
-        String dayn = new String(String.valueOf(cal.get(Calendar.DAY_OF_YEAR)));
+        String weekn = String.valueOf(cal.get(Calendar.WEEK_OF_YEAR));
+        String dayn = String.valueOf(cal.get(Calendar.DAY_OF_YEAR));
         TextView weekday = (TextView)findViewById(R.id.weekandday);
         weekday.setText("Week " + weekn + ", Day " + dayn + ", Year " + year);
 
     }
     String month(int mon){
-        String name = new String();
+        String name = "";
         if(mon == 1) {name = "January";}
         if(mon == 2) {name = "February";}
         if(mon == 3) {name = "March";}
@@ -40,7 +43,7 @@ public class Home extends AppCompatActivity {
         return name;
     }
     String prefix(int day){
-        String pref = new String();
+        String pref;
         if (day%10 == 1) pref = "st";
         else if (day%10 == 2) pref = "nd";
         else if (day%10 == 3) pref = "rd";
